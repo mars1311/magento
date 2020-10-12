2dfddd046897e5d7afefe4945853beb5b760db78
Mariam <servatmandmariam@gmail.com>
2020-10-12 19:14:39 +0300

    Add new textarea with subscribe method

--- app/design/frontend/Mars/customtheme/Magento_Checkout/web/js/custom-step.js
+++ app/design/frontend/Mars/customtheme/Magento_Checkout/web/js/custom-step.js
@@ -6,14 +6,38 @@ define(
         'Magento_Checkout/js/model/step-navigator'
     ],
     function (ko, Component, _, stepNavigator) {
+
+
         return Component.extend({
             defaults: {
                 template: 'Magento_Checkout/mystep',
                 customText: ko.observable('some text from js '),
+                customValue: ko.observable(''),
+
             },
+
             isVisible: ko.observable(true),
+
             initialize: function () {
                 this._super();
+
+                ko.subscribable.fn.subscribeChanged = function (callback) {
+                    let oldValue;
+
+                    this.subscribe(function (_oldValue) {
+                        oldValue = _oldValue;
+                    }, this, 'beforeChange');
+
+                    this.subscribe(function (newValue) {
+                        callback(newValue, oldValue);
+                    });
+                };
+
+                this.customValue.subscribeChanged(function (newValue, oldValue) {
+                    console.log("Новое значение " + newValue + "Старое значение "+ oldValue);
+                });
+
+
                 // register your step
                 stepNavigator.registerStep(
                     //step code will be used as step content id in the component template
